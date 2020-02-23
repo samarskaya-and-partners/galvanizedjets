7b4c02d5a6dd6fa97c69aed9302cfb6ffbfbed44
Simon Cozens <simon@simon-cozens.org>
2020-02-23 19:06:47 +0000

    A new wordlist maker, and new words
    The wordlist maker now tries to get the desired pairs in the middle of the words - but the code which selects the words inside of functions.js doesn't check that yet. Bigger wordlists, but hopefully more interesting ones.

--- utils/compress-wordlist.js
+++ utils/compress-wordlist.js
@@ -1,34 +1,70 @@
 /*
-  Given a wordlist as a JSON array on stdin, output a smaller
-  wordlist containing at least N options for each letter pair.
+  Given a wordlist as text file on stdin, output a smaller
+  wordlist containing at least N options for each letter pair
+  with desiredLeftContext letters to the left and desiredRightContext
+  letters to the right.
 
   Does not do anything clever to find a minimal set, because if
   we have >N options per pair that's a nice bonus. Instead it
-  finds the *first* N options. This is quite helpful because if
-  the wordlist is sorted in order of frequency, then the output will
-  also contain high frequency words.
+  finds the *first* N options. If the list is frequency sorted, this
+  means you'll get familiar samples first. If doShuffle is true, shuffles
+  the array before filtering. This can be used to create more interesting
+  samples in a frequency-sorted list.
 
   Call as
-    node compress-wordlist.js "abcdefghijklmnopqrstuvwxyz" < in.json > out.json
+    node compress-wordlist.js "abcdefghijklmnopqrstuvwxyz" < in.txt > out.json
 */
 
-var minimumWordLength = 3;
-var wordOptions       = 3;
+var wordOptions         = 5;
+var desiredLeftContext  = 3;
+var desiredRightContext = 3;
+var surelySomeMistake   = 15;
+var doShuffle = true;
+
 var alphabet = process.argv[2];
 if (!alphabet) {
   console.error("No alphabet provided");
   process.exit(1)
 }
 
+console.error("Loading dictionary...");
 var fs = require('fs');
-var obj = JSON.parse(fs.readFileSync(process.stdin.fd, 'utf8'));
+var obj = fs.readFileSync(process.stdin.fd, 'utf8').split(/\n/).map( (x) => x.toLowerCase() ).filter( (x) => x.length < surelySomeMistake );
 var marked = {}
+
+if (doShuffle) {
+  console.error("Shuffling array...");
+  for (let i = obj.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [obj[i], obj[j]] = [obj[j], obj[i]];
+  }
+}
+
+
 for (l of alphabet) {
-  console.error(l)
   for (r of alphabet) {
-    var matches = obj.filter( (x) => x.match(l+r) && x.length > minimumWordLength ).slice(0,wordOptions+1);
-    for (m of matches) {
-      marked[m] = true;
+    process.stderr.write(l+r+": ");
+    var matchCount = 0;
+    var matches = obj.filter( (x) => x.includes(l+r) );
+    var tickTock = true;
+    var currentLeft = desiredLeftContext;
+    var currentRight = desiredRightContext;
+    while (matches && matchCount < wordOptions && (currentLeft + currentRight) > 0) {
+      process.stderr.write("-".repeat(currentLeft)+l+r+"-".repeat(currentRight)+" ");
+      contextualMatches = matches.filter( (x) => x.slice(currentLeft,-currentRight).includes(l+r) ).slice(0,wordOptions)
+      matchCount += contextualMatches.length;
+      if (tickTock) { currentRight-- } else { currentLeft-- }
+      tickTock = !tickTock;
+      for (m of contextualMatches) {
+        marked[m] = true;
+      }
+      process.stderr.write(matchCount+" ");
+    }
+    process.stderr.write("\n");
+
+    /* Fake it */
+    if (matchCount == 0) {
+      marked["-".repeat(desiredLeftContext)+l+r+"-".repeat(desiredRightContext)] = true;
     }
   }
 }
