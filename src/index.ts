6c17c4bbc6a7f65e1e1fe328f23ca8607b758a61
Simon Cozens <simon@simon-cozens.org>
2020-02-23 15:58:04 +0000

    Add code to read and parse varfont axes
    Doesn't actually do anything with it yet though

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { setupMovingParts } from './movingparts'
 import { setupFunctions,lazyLoadWordlist } from './functions'
+import * as opentype from 'opentype.js';
 
 import * as $ from "jquery";
+declare var VariableFont;
 
 $( () => {
   setupMovingParts();
@@ -14,7 +16,30 @@ $( () => {
   })
 })
 
+// from StackOverflow, of course...
+function _base64ToArrayBuffer(base64) {
+    var binary_string = window.atob(base64);
+    var len = binary_string.length;
+    var bytes = new Uint8Array(len);
+    for (var i = 0; i < len; i++) {
+        bytes[i] = binary_string.charCodeAt(i);
+    }
+    return bytes.buffer;
+}
+
 window["fontDropCallback"] = function(newFont) {
   var css = `"${newFont.title}", Jets, "Adobe NotDef"`
   $("#fontdrop_view").css("font-family", css)
+
+  // @ts-ignore: CSS stylesheets are messy.
+  var fontUrl = document.styleSheets[1].rules[0].style.src;
+  fontUrl = fontUrl.slice(4, -1) // url( ... )
+  var fontData = _base64ToArrayBuffer(fontUrl.split(",")[1]);
+  var font = opentype.parse(fontData);
+
+  var varFont = new VariableFont(font);
+  var axes = varFont.getAxes()
+  if (axes.length > 0) {
+    // XXX do a thing
+  }
 }
