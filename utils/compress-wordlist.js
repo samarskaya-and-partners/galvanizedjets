e29bd2721c51ca87ffe798795c5b0565a9e7ca46
Simon Cozens <simon@simon-cozens.org>
2020-02-24 10:50:41 +0000

    More dodgy heuristics

--- utils/compress-wordlist.js
+++ utils/compress-wordlist.js
@@ -11,26 +11,64 @@
   the array before filtering. This can be used to create more interesting
   samples in a frequency-sorted list.
 
+  If the --strip-accents flag is passed, then accented letters are not
+  considered "separate letters" for the purposes of building a representative
+  alphabet. (For example, é and e are clearly different in French, but
+  ά and α in Greek not so much.)
+
   Call as
-    node compress-wordlist.js "abcdefghijklmnopqrstuvwxyz" < in.txt > out.json
+    node compress-wordlist.js [--strip-accents] < in.txt > out.json
 */
 
 var wordOptions         = 5;
 var desiredLeftContext  = 3;
 var desiredRightContext = 3;
 var surelySomeMistake   = 15;
+var outlierCutoff       = 0.01;
 var doShuffle = true;
-
-var alphabet = process.argv[2];
-if (!alphabet) {
-  console.error("No alphabet provided");
-  process.exit(1)
-}
+var addDummies = false;
+var killAccents = process.argv[2] == "--strip-accents";
 
 console.error("Loading dictionary...");
 var fs = require('fs');
-var obj = fs.readFileSync(process.stdin.fd, 'utf8').split(/\n/).map( (x) => x.toLowerCase() ).filter( (x) => x.length < surelySomeMistake );
+var obj = fs.readFileSync(process.stdin.fd, 'utf8').split(/\n/).filter( (x) => !x.match(/[\p{Lu}]/u) ).filter( (x) => (x.length < surelySomeMistake && x.length > 2) );
 var marked = {}
+
+console.error("Generating alphabet...");
+var alphabet;
+var alphabetHash = {};
+var count = 5000;
+var extractor;
+if (killAccents) {
+  extractor = /([\p{L}])/gu;
+} else {
+  extractor = /([\p{L}][\p{Mark}]*)/gu;
+}
+var totals = 0;
+for (var w of obj) {
+  if (w.length < 4) continue;
+  if (killAccents) { w = w.normalize("NFD") }
+  do {
+    m = extractor.exec(w);
+    if (m) {
+      if (! (m[1] in alphabetHash)) {
+        alphabetHash[m[1]] = 0
+      }
+      alphabetHash[m[1]]++; totals++;
+    }
+  } while (m);
+  if (count-- < 0) { break;}
+}
+
+var mean = totals / Object.keys(alphabetHash).length
+Object.keys(alphabetHash).map(function(key, index) {
+  alphabetHash[key] /= mean;
+});
+// Drop outliers
+alphabet = Object.keys(alphabetHash).filter( (x) => alphabetHash[x] > outlierCutoff )
+alphabet = alphabet.sort( (a,b) => a.normalize("NFD").localeCompare(b.normalize("NFD")) );
+console.error(alphabet.join(""));
+fs.writeFileSync("alphabet.json", JSON.stringify(alphabet));
 
 if (doShuffle) {
   console.error("Shuffling array...");
@@ -57,7 +95,7 @@ function addMatches(matches, currentLeft, currentRight) {
   process.stderr.write("\n");
 
   /* Fake it */
-  if (matchCount == 0) {
+  if (addDummies && matchCount == 0) {
     marked["-".repeat(desiredLeftContext)+l+r+"-".repeat(desiredRightContext)] = true;
   }
 }
