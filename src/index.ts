3b3d547380e0952c67ff784407c3a87b9aee1bfa
Simon Cozens <simon@simon-cozens.org>
2021-12-23 21:46:25 +0000

    Start in paragraph view (plus bonus eslint). Fixes #3

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,23 @@
-import { setupMovingParts } from './movingparts'
-import { setupFunctions,lazyLoadWordlist } from './functions'
-import { handleNewVariableFont } from './varfonts'
+import { setupMovingParts } from "./movingparts";
+import { setupFunctions, lazyLoadWordlist } from "./functions";
+import { handleNewVariableFont } from "./varfonts";
 import * as $ from "jquery";
 
-$( () => {
+$(() => {
   setupMovingParts();
   setupFunctions();
   lazyLoadWordlist("en", () => {
     /* Start defaults */
-    $("#btn_latin").click()
-    $("#uppercase_pie").click()
-    $("#hobonop input").click()
-  })
-})
+    $("#btn_latin").click();
+    $("#uppercase_pie").click();
+    $("#simple input").click();
+    $("#simple").click();
+  });
+});
 
-window["fontDropCallback"] = function(newFont) {
-  var css = `"${newFont.title}", Jets, "Adobe NotDef"`
-  $("#fontdrop_view").css("font-family", css)
+window["fontDropCallback"] = function (newFont) {
+  var css = `"${newFont.title}", Jets, "Adobe NotDef"`;
+  $("#fontdrop_view").css("font-family", css);
 
   handleNewVariableFont();
-}
+};
